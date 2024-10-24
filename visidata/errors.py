753a7402749358949ea0f73ba3ba868ba213b5b0
midichef <67946319+midichef@users.noreply.github.com>
2024-10-24 14:32:01 -0700

    [errors-] options.debug_stacktrace_full for more callstack

--- visidata/errors.py
+++ visidata/errors.py
@@ -3,6 +3,7 @@ import traceback
 from visidata import vd, VisiData
 
 vd.option('debug', False, 'exit on error and display stacktrace')
+vd.option('debug_stacktrace_full', False, 'for exceptions, show callstack levels above the catch')
 
 
 class ExpectedException(Exception):
@@ -11,10 +12,18 @@ class ExpectedException(Exception):
 
 
 def stacktrace(e=None):
+    '''if options.debug_stacktrace_full is True, display all callstack
+    levels above the level where the exception was caught, in addition
+    to the standard exception.'''
     if not e:
-        return traceback.format_exc().strip().splitlines()
+        if vd.options.debug_stacktrace_full:
+            trim_levels = 3   # calling function -> stacktrace() -> format_stack()
+            stack = ''.join(traceback.format_stack()).strip().splitlines()
+            trace_above = stack[:-2*trim_levels]
+        else:
+            trace_above = []
+        return trace_above + traceback.format_exc().strip().splitlines()
     return traceback.format_exception_only(type(e), e)
-
 
 @VisiData.api
 def exceptionCaught(vd, exc=None, status=True, **kwargs):
